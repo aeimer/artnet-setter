22d2b197b163753cfa4c9801b8f00c768eeb69c7
Alexander Eimer <alexander.eimer@gmail.com>
2021-12-18 21:38:23 +0100

    Status quo

--- startup.js
+++ startup.js
@@ -5,27 +5,28 @@ var options = {
 var artnet = require('artnet')(options);
 
 var universum = 1;
-var fadeTime = 3000;
-var updateRate = 50;
+var fadeTime = 10000;
+var updateRate = 500;
 var startTime = new Date().getTime();
 var target = [
 100,255,175,10,0,0,0,0,
 100,255,175,10,0,0,0,0,
 100,255,175,10,0,0,0,0,
 255,230,0,255,100,0,0,0,
-100,255,175,10,0,0,0,0,
+80,255,65,10,0,0,0,0,
 ];
 
 var fnc = function() {
     var tDiff = (new Date().getTime()) - startTime;
-//    console.log(tDiff);
+    console.log(tDiff);
     if (tDiff > fadeTime) {
         artnet.set(universum, 1, target, function(err, res) {
             artnet.close();
             process.exit();
         });
     }
-    var factor = tDiff / 3000;
+    var factor = tDiff / fadeTime;
+    console.log(factor);
     var targetTmp = target;
     targetTmp = targetTmp.map(x => {
         x = x * factor;
